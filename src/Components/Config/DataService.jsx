71ca15eadf379bbaabe0790faaae700b9aae7b4b
Adam29C <adamcatholic29@gmail.com>
2025-01-15 11:48:59 +0530

    update base url

--- src/Components/Config/DataService.jsx
+++ src/Components/Config/DataService.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 // export const BASE_URL = "http://16.170.215.79:4000/api/v1/"
 // export const BASE_URL = "http://16.171.25.99:5200/";
 // export const BASE_URL = "http://192.168.1.9:5200/";
-export const BASE_URL = "http://3.10.226.224:6999/";
+export const BASE_URL = "http://35.181.15.128:6999/"
 
 const dataservice = axios.create({
   baseURL: BASE_URL,
