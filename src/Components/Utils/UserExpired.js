e38b4b69586d0a30753e96e53ab64be9b7838a09
Adam29C <adamcatholic29@gmail.com>
2025-03-26 17:03:15 +0530

    update build

--- src/Components/Utils/UserExpired.js
+++ src/Components/Utils/UserExpired.js
@@ -1,16 +1,16 @@
 import { jwtDecode } from "jwt-decode";
 
 export const GetExpired = (tokenExpiry, navigate) => {
-  // const decoded = jwtDecode(tokenExpiry && tokenExpiry);
+  const decoded = jwtDecode(tokenExpiry && tokenExpiry);
 
-  // if (decoded.exp < parseInt(Date.now() / 1000)) {
-  //   // navigate("/tokenexpiry", { replace: true });
-  //   navigate("/", { replace: true });
-  //   localStorage.removeItem("token");
-  //   localStorage.removeItem("userdetails");
-  //   // setTimeout(() => {
-  //   // }, 1000);
-  // } else {
-  //   return "not Expired";
-  // }
+  if (decoded.exp < parseInt(Date.now() / 1000)) {
+    // navigate("/tokenexpiry", { replace: true });
+    navigate("/", { replace: true });
+    localStorage.removeItem("token");
+    localStorage.removeItem("userdetails");
+    // setTimeout(() => {
+    // }, 1000);
+  } else {
+    return "not Expired";
+  }
 };
