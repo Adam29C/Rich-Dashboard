c0d75cef6f18ba15b39cd9ce2fb21c99128d2f8e
Adam29C <adamcatholic29@gmail.com>
2025-04-07 12:24:39 +0530

    fix date filter issue

--- src/Components/Pages/Superadmin/Reports/GatwayHistory.jsx
+++ src/Components/Pages/Superadmin/Reports/GatwayHistory.jsx
@@ -115,7 +115,31 @@ const MainGameReports = ({
         );
 
         if (res.status) {
-          setTableData(res.data);
+          let aarrrr = [];
+          res.data.forEach((item) => {
+            let dateObj = new Date(item.created_at);
+
+            let formattedDate = dateObj.toLocaleString("en-IN", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+              hour12: true,
+            });
+
+            aarrrr.push({ ...item, created_at: formattedDate });
+            return;
+          });
+
+          aarrrr.sort(
+            (a, b) => parseDate(b.created_at) - parseDate(a.created_at)
+          );
+
+          console.log("aarrrr", aarrrr);
+
+          setTableData(aarrrr);
         }
       } catch (error) {
         console.log(error);
