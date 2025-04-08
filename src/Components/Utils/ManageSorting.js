4c85fca74069af93160f67e9f50e3fda81ede7ab
Adam29C <adamcatholic29@gmail.com>
2025-04-08 15:24:57 +0530

    update build

--- src/Components/Utils/ManageSorting.js
+++
@@ -1,5 +0,0 @@
-export const parseDate = (dateString) => {
-  const [datePart, timePart] = dateString.split(", ");
-  const [day, month, year] = datePart.split("/");
-  return new Date(`${year}-${month}-${day} ${timePart}`);
-};
